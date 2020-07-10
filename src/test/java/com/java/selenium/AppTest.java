a9aefb075643d9eb65d9ebc0f760b3434159180e
rinkucool007 <pratap_the_knight@yahoo.com>
2020-07-09 22:06:41 -0400

    Add files via upload

---
+++ src/test/java/com/java/selenium/AppTest.java
@@ -0,0 +1,55 @@
+package com.java.selenium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class AppTest  {
+	
+	public WebDriver driver;
+
+  @Test
+  public void openMyBlog() {
+	driver.get("https://www.google.com/");
+  }
+
+    @BeforeTest
+    public void setupDriver(ITestContext ctx) throws MalformedURLException {
+        // BROWSER => chrome / firefox
+        // HUB_HOST => localhost / 10.0.1.3 / hostname
+
+        String host = "192.168.182.140";
+        DesiredCapabilities dc;
+
+        if(System.getProperty("BROWSER") != null &&
+                System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
+            dc = DesiredCapabilities.firefox();
+        }else{
+            dc = DesiredCapabilities.chrome();
+        }
+
+        if(System.getProperty("HUB_HOST") != null){
+            host = System.getProperty("HUB_HOST");
+        }
+
+        String testName = ctx.getCurrentXmlTest().getName();
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+        dc.setCapability("name", testName);
+        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+    }
+  
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+}
